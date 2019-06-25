90c1178979a4a90be251438e21f0b8b9254b950b
Connor Jones <ConnorLeeJones@gmail.com>
2019-06-25 08:35:25 -0400

    109 passing

--- src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
+++ src/main/java/rocks/zipcode/io/quiz3/generics/ArrayUtility.java
@@ -1,6 +1,12 @@
 package rocks.zipcode.io.quiz3.generics;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author leon on 09/12/2018.
@@ -8,28 +14,79 @@ import java.util.function.Function;
 public class ArrayUtility<SomeType> {
     private final SomeType[] array;
 
+    //private final Class<SomeType> type;
+
+
     public ArrayUtility(SomeType[] array) {
         this.array = array;
     }
 
     public SomeType findOddOccurringValue() {
-        SomeType type;
         for (SomeType t : array){
-
+            if (getNumberOfOccurrences(t) % 2 == 1){
+                return t;
+            }
         }
-
         return null;
     }
 
     public SomeType findEvenOccurringValue() {
+        for (SomeType t : array){
+            if (getNumberOfOccurrences(t) % 2 == 0){
+                return t;
+            }
+        }
         return null;
     }
 
     public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
-        return null;
+        Integer count = 0;
+        for (SomeType t : array){
+            if (t.equals(valueToEvaluate)){
+                count++;
+            }
+        }
+        return count;
     }
 
     public SomeType[] filter(Function<SomeType, Boolean> predicate) {
+        List<SomeType> toFilter = Arrays.asList(array);
+        List<SomeType> filtered = toFilter.stream().filter(f -> predicate.equals(true)).collect(Collectors.toList());
+
+                //.filter(filtered -> )
+                ;
+        //SomeType[] result = new SomeType[filtered.size()];
+
+
+        //SomeType[] result = (SomeType[]) filtered.toArray();
+
+
+
+        //List<SomeType> toFilter = Arrays.asList(array);
+        //SomeType[] newArr = (SomeType[]) Arrays.stream(array).filter(f -> predicate.equals(true)).toArray();
+        //SomeType[] result = (SomeType[]) Array.newInstance(Class<SomeType> type, filtered.size());
+
+        //SomeType[] newArr = genericConvert(this.type, filtered);
+
         return null;
     }
+
+
+
+    public static <SomeType> SomeType[] genericConvert(Class<SomeType> type, List<SomeType> input){
+
+        @SuppressWarnings("unchecked")
+        SomeType[] result = (SomeType[]) Array.newInstance(type, input.size());
+
+        int i = 0;
+        for (SomeType t : input){
+            result[i] = t;
+            i++;
+        }
+        return result;
+    }
+
+
+
+
 }
